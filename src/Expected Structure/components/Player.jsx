0866ed4321430da954a92886de83c08ed66843e3
Ichchuk Adhikari <aichchuk@gmail.com>
2025-07-16 16:56:44 +0545

    basic components

--- src/Expected Structure/components/Player.jsx
+++ src/Expected Structure/components/Player.jsx
@@ -9,28 +9,40 @@ import {
 
 const Player = () => {
   return (
-    <div className="fixed bottom-0 left-6 right-6 h-[90px] bg-[rgba(43,43,43,0.868)] rounded-t-2xl shadow-[0_-4px_20px_rgba(27,27,27,0.559)] flex items-center justify-between px-8 text-white font-medium text-base z-[150] gap-4">
+    <div className="fixed bottom-0 left-6 right-6 h-[90px] bg-[rgba(43,43,43,0.868)] rounded-t-2xl shadow-[0_-4px_20px_rgba(27,27,27,0.559)] flex items-center justify-between px-10 text-white font-medium text-base z-[150] w-auto gap-x-6">
       
       {/* Music Info */}
-      <div className="flex flex-col justify-center min-w-[150px]">
-        <p className="m-0 font-bold text-sm">Now Playing</p>
-        <span className="text-xs text-[#ffd6cc]">Artist – Song Title</span>
+      <div className = 'flex items-center justify-between gap-4'>
+        <img
+            src="/src/Expected Structure/assets/images/Original audio.jpg"
+            className="w-12 h-12 rounded-md object-cover"
+          />
+        <div className="flex flex-col justify-center min-w-[150px]">
+          <p className="m-0 font-bold text-sm font-sans">Now Playing</p>
+          <span className="text-xs font-semibold text-[#ffd6cc]">Artist – Song Title</span>
+        </div>  
       </div>
+      
 
       {/* Player Controls */}
       <div className="flex items-center gap-8">
         <FaStepBackward className="text-xl cursor-pointer transition-transform duration-200 hover:scale-125" />
-        <FaPlay className="text-2xl text-[#ffffff] cursor-pointer transition-transform duration-200 hover:scale-125" />
-        {/* Replace <FaPlay /> with <FaPause /> for pause state */}
+        <FaPlay className="text-2xl text-white cursor-pointer transition-transform duration-200 hover:scale-125" />
+        {/* Replace with <FaPause /> to indicate pause */}
         <FaStepForward className="text-xl cursor-pointer transition-transform duration-200 hover:scale-125" />
       </div>
 
       {/* Volume & Progress */}
       <div className="flex items-center gap-6 min-w-[220px]">
         {/* Progress Bar */}
-        <div className="bg-white/20 h-[6px] w-[120px] rounded-full overflow-hidden">
-          <div className="w-[40%] h-full bg-white" />
+        <div className='flex items-center gap-2'>
+          <div className='text-white text-[13px]'> 1:53 </div>
+          <div className="bg-white/20 h-[6px] w-[120px] rounded-full overflow-hidden">
+            <div className="w-[70%] h-full bg-white" />
+          </div>
+          <div className='text-white text-[13px]'> 3:24 </div>
         </div>
+        
 
         {/* Volume Control */}
         <div className="flex items-center gap-2">
---
+++ src/Expected Structure/components/TextButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextButton = () => {
+  return (
+    <div className = ' flex justify-center items-center '>
+        <button className = ' text-white font-sans font-extrabold px-10 py-2 outline-2 rounded-full hover:bg-white hover:text-black text-16px'>Display</button>
+    </div>
+  )
+}
+
+export default TextButton
