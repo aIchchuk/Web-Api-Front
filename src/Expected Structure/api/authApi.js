cdc001189d712f626ddb5fc10566b062e6d44b5a
Ichchuk Adhikari <aichchuk@gmail.com>
2025-07-18 19:28:32 +0545

    auth

--- src/Expected Structure/api/authApi.js
+++ src/Expected Structure/api/authApi.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import { axiosInstance } from "./api";
 
-export const registerApi = (data) => axios.post("/auth/register", data)
-export const loginApi = (data) => axios.post("/auth/login", data)
+export const registerApi = (data) => axiosInstance.post("/auth/register", data)
+export const loginApi = (data) => axiosInstance.post("/auth/login", data)
--- src/Expected Structure/services/authService.js
+++ src/Expected Structure/services/authService.js
@@ -1,12 +1,13 @@
 // services/authService.js
-import { axiosInstance } from "../api/api";
+import { loginApi, registerApi } from "../api/authApi";
 
 export const loginUserService = async ({ email, password }) => {
-  const res = await axiosInstance.post("/auth/login", { email, password });
+  // const res = await axiosInstance.post("/auth/login", { email, password });
+  const res = await loginApi({email, password});
   return res.data; // This should contain { message, token, user }
 };
 
 export const registerUserService = async ({ fullName, email, password }) => {
-  const res = await axiosInstance.post("/auth/register", { fullName, email, password });
+  const res = await registerApi({fullName, email, password});
   return res.data; // This should contain { message, user }
 };
